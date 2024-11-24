c07ea8e70cfa0ed94870c12ac5423b53505cef8d
Polad Mammadov <pmammadov18@gmail.com>
2024-11-24 14:07:28 +0100

    Second Commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         //Add new code here
+
+
+        System.out.println("This my branch");
+
     }
 }
